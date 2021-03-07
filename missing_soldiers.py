33729742dc2424655cc7348f0e6b2213ae59547d
Mthabisi Ndlovu <26140169+codymndlovu@users.noreply.github.com>
2021-03-07 10:48:55 +0200

    Code refactor- used list destructuring and renamed variables.

--- missing_soldiers.py
+++ missing_soldiers.py
@@ -1,18 +1,18 @@
 def count_blocked(barriers):
     barriers.sort(key=lambda x: x[0])
-    num_blocked, last_blocked = barriers[0][1] + 1 - barriers[0][0]
+    num_blocked = barriers[0][1] + 1 - barriers[0][0]
     last_blocked = barriers[0][1]
 
     for i in range(1, len(barriers)):
-        if barriers[i][1] <= lastblocked:
+        if barriers[i][1] <= last_blocked:
             continue
 
-        x1, x2 = barriers[i][0], barriers[i][1]
-        if barriers[i][0] > lastblocked:
+        x1, x2 = barriers[i]
+        if barriers[i][0] > last_blocked:
             num_blocked += x2 + 1 - x1
         else:
-            num_blocked += x2 - lastblocked
-        lastblocked = x2
+            num_blocked += x2 - last_blocked
+        last_blocked = x2
     return num_blocked
         
 
